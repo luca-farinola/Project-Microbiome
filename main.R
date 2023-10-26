19c974f9f8a244c345a2105bf5973419be0711c3
luca-farinola <farinola.1898853@studenti.uniroma1.it>
2023-10-26 14:06:08 +0200

    performing Random Foresd in main

--- main.R
+++ main.R
@@ -8,13 +8,14 @@ setwd("C:/Users/lukfa/OneDrive/Desktop/Project-Microbiome")
 source("scripts/libraries.R")
 source("scripts/filter_and_normalize.R")
 source("scripts/mean_centering.R")
+source("scripts/3dplot.R")
 
 # file importing 
 asv <- read.csv("files/finASV.csv", header = TRUE, row.names = 1)
 samp <- read.csv("files/finSAMP.csv", header=TRUE, stringsAsFactors = TRUE, row.names = 1)
 taxa <- read.csv("files/finTAXA.csv", header = TRUE, row.names = 1)
 
-filter_and_normalize <- process_data(asv, taxa, samp)
+filter_and_normalize <- filter_and_normalize(asv, taxa, samp)
 data <- filter_and_normalize$data
 metadata <- filter_and_normalize$metadata
 
@@ -41,29 +42,51 @@ ResPCA_after <- pcaBySvd(as.matrix(mean_centered))
 
 pcaScorePlot(
   ResPCA_after,
-  axes = c(1, 3),
+  axes = c(2, 3),  # i have tried different axes
   title = "PCA mean centered data",design = metadata,
   color = "patient", shape = "patient_type"
 )
 
-#Unsupervised Random Forest 
+#probably still a great separation on timepoint 
 
+pcaScorePlot(
+  ResPCA_after,
+  axes = c(1, 2), # i have tried different axes 
+  title = "PCA mean centered data",design = metadata,
+  color = "time_point"
+)
+
+
+# Unsupervised Random Forest
+
+seed(123)
 iris.urf <- randomForest(mean_centered)
+MDSplot(iris.urf,metadata$patient_type,k=5,palette=NULL)
+
 a <- MDSplot(iris.urf,metadata$patient_type,k=5,palette=NULL)
+MDS <- a$points
 
-MDS <- a$points 
+plot3d(MDS[,3:5], xlab="Component 3", ylab="Component 4", zlab="Component 5", type="n", box=F, axes=F)
 
-# 3D plots (time_points and patient type)
+decorate3d( xlab="Component 3", ylab="Component 4", zlab="Component 5", 
+            box = TRUE, axes = TRUE, main = NULL, sub = NULL,
+            top = TRUE, aspect = FALSE, expand = 1.03)
 
-# Color according to patient type 
+spheres3d(MDS[,3:5], radius=0.009, col= patient_type_labels)
 
-patient_type_mapping <- c("HC" = "green","NCWS" = "red")
-patient_type_labels <- patient_type_mapping[metadata$patient_type]
 
-# Color according to Time Point 
 
-time_point_mapping <- c("T0" = "blue","T5" = "yellow")
-time_point_labels <- time_point_mapping[metadata$time_point]
+
+
+
+
+
+
+
+
+
+
+
 
 
 
